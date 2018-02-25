e0c1980a48032797b77c4c4027e953bf1d5467f9
zecollokaris <collo.kariss@gmail.com>
2018-02-25 22:54:29 +0300

    saving to git hub

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document) .ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+       });
+
+       $("button#red").click(function() {
+         $("body").removeClass();
+         $("body").addClass("red-background");
+       });
+     });
